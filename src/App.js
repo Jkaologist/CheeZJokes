3af7e0a74754205f9876bc96b0282705e1eb39f1
Jon K <Johnakao@gmail.com>
2021-04-16 14:34:53 -0700

    Refactored!

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import JokeList from "./JokeList";
 
 /** App component. Renders list of jokes. */
 
-function App () {
-    return (
-      <div className="App">
-        <JokeList />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
 }
 
 export default App;
